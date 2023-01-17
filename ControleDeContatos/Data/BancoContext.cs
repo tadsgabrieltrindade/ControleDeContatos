86dccf400fdbcf75d5220912f7828ffe5b909584
icand <icand@DESKTOP-5K3TE17>
2023-01-16 21:44:06 -0300

    Adicionar arquivos de projeto.

---
+++ ControleDeContatos/Data/BancoContext.cs
@@ -0,0 +1,17 @@
+﻿using ControleDeContatos.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ControleDeContatos.Data
+{
+    public class BancoContext : DbContext
+    {
+        //Construdor
+        public BancoContext(DbContextOptions <BancoContext> options) : base(options) 
+        { 
+               
+        }   
+
+        //Configuração do contexto - No caso, cria um banco de dados do com o nome Contato
+        public DbSet<ContatoModel> Contato { get; set; }
+    }
+}
